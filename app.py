aa7f5a4346bbcfe954297ac92fb751df22e41b26
Ariel Hsieh <65921543+arielhsieh8@users.noreply.github.com>
2023-04-09 16:30:34 -0400

    update app.py

--- app.py
+++ app.py
@@ -1,1 +1,17 @@
+import streamlit as st  #Web App
+from transformers import pipeline
+
+#title
+st.title("Sentiment Analysis - Classify Sentiment of text")
+
+data = []
+text = st.text_input("Enter text here:")
+data.append(text)
+
+sentiment_pipeline = pipeline("sentiment-analysis")
+
+result = sentiment_pipeline(data)
+
+st.write(result)
+
 
