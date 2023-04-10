4b8f05e8f2afe0556c1c7652ff414246eff239c2
Ariel Hsieh <65921543+arielhsieh8@users.noreply.github.com>
2023-04-09 20:38:24 -0400

    update app.py

--- app.py
+++ app.py
@@ -2,12 +2,10 @@ import streamlit as st  #Web App
 from transformers import pipeline
 from pysentimiento import create_analyzer
 
-
-
-model = st.selectbox("Which pretrained model would you like to use?",("DistilBERT","twitter-XLM-roBERTa-base","bertweet-sentiment-analysis"))
-
 #title
 st.title("Sentiment Analysis - Classify Sentiment of text")
+
+model = st.selectbox("Which pretrained model would you like to use?",("roberta-large-mnli","twitter-XLM-roBERTa-base","bertweet-sentiment-analysis"))
 
 data = []
 text = st.text_input("Enter text here:","Artificial Intelligence is useful")
@@ -15,8 +13,8 @@ data.append(text)
 if model == "DistilBERT":
     #1
     if st.button("Run Sentiment Analysis of Text"): 
-        model_path = "distilbert-base-uncased-finetuned-sst-2-english"
-        sentiment_pipeline = pipeline("sentiment-analysis",model=model_path, tokenizer=model_path)
+        model_path = "roberta-large-mnli"
+        sentiment_pipeline = pipeline(model=model_path)
         result = sentiment_pipeline(data)
         label = result[0]["label"]
         score = result[0]["score"]
@@ -27,7 +25,7 @@ elif model == "twitter-XLM-roBERTa-base":
         model_path = "cardiffnlp/twitter-xlm-roberta-base-sentiment"
         sentiment_task = pipeline("sentiment-analysis", model=model_path, tokenizer=model_path)
         result = sentiment_task(text)
-        label = result[0]["label"]
+        label = result[0]["label"].capitalize()
         score = result[0]["score"]
         st.write("The classification of the given text is " + label + " with a score of " + str(score))
 
@@ -46,7 +44,7 @@ elif model == "bertweet-sentiment-analysis":
         neg = result.probas["NEG"]
         pos = result.probas["POS"]
         neu = result.probas["NEU"]
-        st.write("The classification of the given text is " + label + " with the percentages broken down as: Positive - " + str(pos) + ", Neutral - " + str(neu) + ", Negative - " + str(neg))
+        st.write("The classification of the given text is " + label + " with the scores broken down as: Positive - " + str(pos) + ", Neutral - " + str(neu) + ", Negative - " + str(neg))
 
 
 
